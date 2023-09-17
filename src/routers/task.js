a832ae35ddf56d1de4961835d4f30f2bd1f1f21c
MOHIT SAINI <mohitsaininith@gmail.com>
2023-09-17 15:05:11 +0530

    Done sorting,pagination & filtering of GET /task API

--- src/routers/task.js
+++ src/routers/task.js
@@ -3,10 +3,29 @@ const Task = require('../models/task');
 const auth = require('../middleware/auth');
 const router = new express.Router;
 
-
+//GET /tasks?completed=true
+//GET /tasks?limit=10&skip=10
+//GET /tasks?sortBy=createdAt:desc
 router.get('/tasks',auth,async(req,res)=>{
+    const match = {};
+    const sort = {};
+    if(req.query.completed){
+        match.completed = (req.query.completed=='true');
+    }
+    if(req.query.sortBy){
+        const parts = req.query.sortBy.split(':');
+        sort[parts[0]] = (parts[1]==='asc'?1:-1);
+    }
     try{
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path:'tasks',
+            match,
+            options:{
+                limit:parseInt(req.query.limit),
+                skip:parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
         res.send(req.user.tasks);
     }catch(e){
         res.status(500).send(e);
@@ -61,8 +80,6 @@ router.patch('/tasks/:id',auth,async(req,res)=>{
 })
 
 router.delete('/tasks/:id',auth,async (req,res)=>{
-    console.log(req.params.id);
-    console.log(req.user._id);
     try{
         const task = await Task.findOneAndDelete({_id:req.params.id,owner:req.user._id});
         if(!task){
