d042259b1e8bd04b360a064e1e1b405c0f412f15
MOHIT SAINI <mohitsaininith@gmail.com>
2023-09-18 23:51:08 +0530

    Added Apis for profile pic upload

--- src/index.js
+++ src/index.js
@@ -8,7 +8,6 @@ require('./db/mongoose');
 const app = express();
 const port = (process.env.port || 3000);
 
-
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,9 @@ const express = require('express');
 const User = require('../models/user');
 const auth = require('../middleware/auth');
 const router = new express.Router;
+const sharp = require('sharp');
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
 
 router.get('/users/me',auth,async (req,res)=>{
    res.send(req.user);
@@ -59,7 +61,7 @@ router.delete('/users/me',auth,async (req,res)=>{
     }
 })
 
-router.post('/users/login',auth,async(req,res)=>{
+router.post('/users/login',async(req,res)=>{
     try{
         const user = await User.findByCredentials(req.body.email,req.body.password);
         const token = await user.generateAuthToken();
@@ -90,4 +92,43 @@ router.post('/users/logoutAll',auth,async(req,res)=>{
         res.status(500).send();
     }
 })
+const multerInstance = multer({
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req,file,callback){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return callback(new Error('please upload an image'));
+        }
+        callback(undefined,true);
+    }
+});
+router.post('/users/me/avatar',auth,multerInstance.single('avatar'),async(req,res)=>{
+    const buffer = await sharp(req.file.buffer).resize({height:500,width:500}).png().toBuffer();
+    req.user.avatar = buffer;
+    await req.user.save();
+    res.send();
+},(error,req,res,next)=>{
+    res.status(400).send({error:error.message});
+})
+
+router.delete('/users/me/avatar',auth,async(req,res)=>{
+    req.user.avatar = undefined;
+    await req.user.save();
+    res.send()
+})
+
+router.get('/users/:id/avatar',async(req,res)=>{
+    try{
+        const user = await User.findById(req.params.id);
+        if(!user || !user.avatar){
+            throw new Error();
+        }
+        res.set('Content-Type','image/jpg');
+        res.send(user.avatar);
+    }catch(e){
+        res.status(400).send();
+    }
+})
+
 module.exports = router;
